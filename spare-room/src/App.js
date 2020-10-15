8df1dee7d868e1004e44cde54024ac00d4d9f25b
Cameron Sellers <cameron.sellers07@gmail.com>
2020-10-15 14:55:27 -0500

    added new packages and menu assestw

--- spare-room/src/App.js
+++ spare-room/src/App.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import {Nav,NavItem} from 'reactstrap'
-
+import {useMediaQuery} from 'react-responsive'
+import Menu from './menu'
 import './App.css';
+
 import {Route} from 'react-router-dom'
 
 
 import HomePage from './components/homepage';
 
 function App() {
-  
+  const isMobile = useMediaQuery({ query: "(max-width:426px)" });
   return (
     <div>
-      <Nav className="Nav">
+      {isMobile ? (
+        <div>
+        <Menu />
+        </div>
+      )
+      : (
+        <div>
+        <Nav className="Nav">
         <NavItem>Services</NavItem>
         <NavItem>Connect</NavItem>
         <NavItem>Projects</NavItem>
       </Nav>
+      </div>
+      )
+
+      }
      
     
     <Route exact path = "/" component={HomePage} />
 
     
     </div>
-  );
+  )
 }
 
 export default App;
--- spare-room/src/components/homepage.js
+++ spare-room/src/components/homepage.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import {useMediaQuery} from 'react-responsive'
+import './homepage.css'
+
+
+
+
 const HomePage = () => {
 
 const isMobile = useMediaQuery({ query: "(max-width:426px)" });
   return (
     <div>
-     
-    
-    
-
-    
     {isMobile ? (
-      <div>
-        <img src="SRWD_HalfLogoType_Black.png" />
+      <div className='logo-container'>
+        <img className='logo' src='https://res.cloudinary.com/drcgo7zqn/image/upload/v1602773562/SpareRoom%20image%20assets/SRWD_HalfLogoType_Black_gdaeao.jpg' />
       </div>
-    ) : (
-      <div>
-        <img src="SRWD_LogoType_Black.png" />
+    ) 
+    : 
+    (
+      <div className='logo-container'>
+        <img className='logo' src='https://res.cloudinary.com/drcgo7zqn/image/upload/v1602773536/SpareRoom%20image%20assets/BlackFull_wpc0qd.png' />
       </div>
-    )
-      
-    }
+    )}
     </div>
   )}
 
